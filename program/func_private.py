7688ad5f82052b6bc1b4a4e2c2d83ce1a7864328
luke goh <realgohluke@gmail.com>
2023-03-13 11:45:19 -0400

    Stage 3 completion

--- program/func_private.py
+++ program/func_private.py
@@ -1,78 +1,135 @@
 from datetime import datetime, timedelta
 from func_utils import format_number
 import time
+import json
 
 from pprint import pprint
 
+# Check order status
+def check_order_status(client, order_id):
+  order = client.private.get_order_by_id(order_id)
+  return order.data["order"]["status"]
+
+
+# Get existing open positions
+def is_open_positions(client, market):
+
+  # Protect API
+  time.sleep(0.2)
+
+  # Get positions
+  all_positions = client.private.get_positions(
+    market=market,
+    status="OPEN"
+  )
+
+  # Determine if open
+  if len(all_positions.data["positions"]) > 0:
+    return True
+  else:
+    return False
+
+
+# Check order status
+def check_order_status(client, order_id):
+  order = client.private.get_order_by_id(order_id)
+  if order.data:
+    if "order" in order.data.keys():
+      return order.data["order"]["status"]
+  return "FAILED"
+
+
 # Place market order
 def place_market_order(client, market, side, size, price, reduce_only):
-    # Get Position Id
-    account_response = client.private.get_account()
-    position_id = account_response.data["account"]["positionId"]
+  # Get Position Id
+  account_response = client.private.get_account()
+  position_id = account_response.data["account"]["positionId"]
 
-    # Get expiration time
-    server_time = client.public.get_time()
-    expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", "")) + timedelta(seconds=70)
+  # Get expiration time
+  server_time = client.public.get_time()
+  expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", "")) + timedelta(seconds=70)
 
-    # Place an order
-    placed_order = client.private.create_order(
-        position_id=position_id, # required for creating the order signature
-        market=market,
-        side=side,
-        order_type="MARKET",
-        post_only=False,
-        size=size,
-        price=price,
-        limit_fee='0.015',
-        expiration_epoch_seconds=expiration.timestamp(),
-        time_in_force="FOK", 
-        reduce_only=reduce_only
-    )
+  # Place an order
+  placed_order = client.private.create_order(
+    position_id=position_id, # required for creating the order signature
+    market=market,
+    side=side,
+    order_type="MARKET",
+    post_only=False,
+    size=size,
+    price=price,
+    limit_fee='0.015',
+    expiration_epoch_seconds=expiration.timestamp(),
+    time_in_force="FOK", 
+    reduce_only=reduce_only
+  )
 
-    # Return result
-    return placed_order.data
+  # print(placed_order.data)
 
-# Abort all open position
+  # Return result
+  return placed_order.data
+
+
+# Abort all open positions
 def abort_all_positions(client):
+  
+  # Cancel all orders
+  client.private.cancel_all_orders()
 
-    # Cancel all orders
-    client.private.cancel_all_orders()
+  # Protect API
+  time.sleep(0.5)
 
-    # Protect API
-    time.sleep(0.5)
+  # Get markets for reference of tick size
+  markets = client.public.get_markets().data
 
-    # Get markets for reference of tick size
-    markets = client.public.get_markets().data
+  # Protect API
+  time.sleep(0.5)
 
-    # Protect API
-    time.sleep(0.5)
+  # Get all open positions
+  positions = client.private.get_positions(status="OPEN")
+  all_positions = positions.data["positions"]
 
-    # Get all open positions
-    positions = client.private.get_positions(status="OPEN")
-    all_positions = positions.data["positions"]
+  # Handle open positions
+  close_orders = []
+  if len(all_positions) > 0:
 
-    pprint(all_positions)
+    # Loop through each position
+    for position in all_positions:
 
-    # Handle open positions
-    close_orders = []
-    if len(all_positions) > 0:
+      # Determine Market
+      market = position["market"]
 
-        # Loop through each position
-        for position in all_positions:
+      # Determine Side
+      side = "BUY"
+      if position["side"] == "LONG":
+        side = "SELL"
 
-            # Determine Market
-            market = position["market"]
+      # Get Price
+      price = float(position["entryPrice"])
+      accept_price = price * 1.7 if side == "BUY" else price * 0.3
+      tick_size = markets["markets"][market]["tickSize"]
+      accept_price = format_number(accept_price, tick_size)
 
-            # Determine Side
-            side = "BUY"
-            if position["side"] == "LONG":
-                side = "SELL"
+      # Place order to close
+      order = place_market_order(
+        client,
+        market,
+        side,
+        position["sumOpen"],
+        accept_price,
+        True
+      )
 
-            # Get Price
-            price = float(position["entryPrice"])
-            accept_price = price * 1.7 if side == "BUY" else price * 0.3
-            tick_size = markets["markets"][market]["tickSize"]
-            accept_price = format_number(accept_price, tick_size)
+      # Append the result
+      close_orders.append(order)
 
+      # Protect API
+      time.sleep(0.2)
 
-    
+    # Override json file with empty list
+    bot_agents = []
+    with open("bot_agents.json", "w") as f:
+      json.dump(bot_agents, f)
+
+    # Return closed orders
+    return close_orders
